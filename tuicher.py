8b9182ed131c08331e339c7e254f847604df6739
Javier Raja <j.rajahuertas@gmail.com>
2021-10-14 03:29:31 +0200

    Updating tuicher.py
    new features

--- tuicher.py
+++ tuicher.py
@@ -1,13 +1,51 @@
+# IMPORT SEGMENT
 from binance.client import Client
+import  time
 import config
 
-client = Client(config.API_KEY, config.API_SECRET, testnet=True)
+# BOT ATRIBUTES
+PING_RATE = 0.5         
+CURRENCY = 'BTCBUSD'
+CYCLE_ITERS = 999999    #1x10^6 iteractions * 10 segs = 115 Days
 
-info = client.get_symbol_info('BNBBTC')
+# Variables
+val = 0
+last_val = 0
 
-prices = client.get_all_tickers()
+client = Client(config.API_KEY, config.API_SECRET, tld='com')
 
-for s in prices:
-    print (s['symbol'], s['price'])
+def getData(key):
+    #client.ping()
+    prices = client.get_all_tickers()
+    for s in prices:
+        if(s['symbol'] ==  key):
+            return s
+
+#Debug
+def printAllData():
+    client.ping()
+    prices = client.get_all_tickers()
+    for p in prices:
+        print(p['symbol'],': ', p['price'])
+
+
+#info = client.get_symbol_info('BNBBTC')
+
+i = 0
+while i < CYCLE_ITERS:
+    value = getData(CURRENCY)
+
+    last_val =  val
+    val =  float(value['price'])
+
+    if (last_val > val):
+        k = '-'
+    else:
+        k = '+'
+
+    print(i,'\t', time.ctime(), value['symbol'],': ', value['price'], k, abs(last_val - val))
+    i+=1
+    time.sleep(PING_RATE)
+
 
 #print(prices)
